9980bb51c428f64f76b7688da8d07833376c37d5
kristiyan2021 <449302@student.saxion.nl>
2022-03-25 12:31:32 +0100

    feat: method visitor added

--- src/nl/saxion/cos/visitor/MethodVisitor.java
+++ src/nl/saxion/cos/visitor/MethodVisitor.java
@@ -1,2 +1,11 @@
-package nl.saxion.cos.visitor;public class MethodVisitor {
+package nl.saxion.cos.visitor;
+
+import nl.saxion.cos.SexyLangBaseVisitor;
+import nl.saxion.cos.SexyLangParser;
+
+public class MethodVisitor extends SexyLangBaseVisitor<Void> {
+    @Override
+    public Void visitBedActivityStmt(SexyLangParser.BedActivityStmtContext ctx) {
+        return super.visitBedActivityStmt(ctx);
+    }
 }
