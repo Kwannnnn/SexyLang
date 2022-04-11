e099a71539090ce8676972998837dd712d3e72fd
Quan <490848@student.saxion.nl>
2022-04-11 17:18:52 +0200

    whatlength tests

--- tests/nl/saxion/cos/UserInputTest.java
+++ tests/nl/saxion/cos/UserInputTest.java
@@ -1,5 +1,7 @@
 package nl.saxion.cos;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,33 +27,132 @@ public class UserInputTest extends TestBase{
     public static final float FLOAT = 42.37F;
     public static final String BOOLEAN = "hard";
 
-    private static final Compiler COMPILER = new Compiler();
+    private Compiler c = new Compiler();
+    private final InputStream oldIn = System.in;
 
-    // not working
-//    @Test
-//    @DisplayName("Good weather whatLength() call")
-//    void goodWeather_whatLengthCall() throws Exception {
-//        ByteArrayInputStream in = new ByteArrayInputStream(floatToByteArray(FLOAT));
-//        System.setIn(in);
-//
-//        // moan "shrek" - 37
-//        String srcCode =
-//                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
-//        // String - Int should not be allowed
-//        JasminBytecode code = COMPILER.compileString(srcCode, "stringMinInt");
-//        code.writeJasminToFile("dasdadasd");
-//        assertNotNull(code, "Subtraction of strings and integers should not be allowed");
-//
-//        // Check that output matches what we expect
-//        List<String> output = runCode(code);
-//        assertArrayEquals(new String[] {
-//                String.valueOf(FLOAT)
-//        }, output.toArray());
-//    }
-
-    public static byte[] floatToByteArray(float value) {
-        int intBits =  Float.floatToIntBits(value);
-        return new byte[] {
-                (byte) (intBits >> 24), (byte) (intBits >> 16), (byte) (intBits >> 8), (byte) (intBits) };
+    @BeforeEach
+    void setUp() {
+        this.c = new Compiler();
     }
+
+    @AfterEach
+    void cleanup() {
+        System.setIn(oldIn);
+    }
+
+    @Test
+    @DisplayName("Good weather whatLength() call")
+    void goodWeather_whatLengthCall() throws Exception {
+        String input = "6,9";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // moan 6.9
+        String srcCode =
+                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
+        JasminBytecode code = this.c.compileString(srcCode, "whatLength");
+        assertNotNull(code);
+
+        // Check that output matches what we expect
+        List<String> output = runCode(code);
+        assertArrayEquals(new String[] {
+                "6.9"
+        }, output.toArray());
+    }
+
+    @Test
+    @DisplayName("Bad weather whatLength() call - invalid type (bulge)")
+    void badWeather_whatLengthCall_invalid_type_bulge() throws Exception {
+        String input = "hard";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // moan hard
+        String srcCode =
+                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
+        JasminBytecode code = this.c.compileString(srcCode, "whatLengthBulge");
+
+        // Check that correct exception is thrown
+        assertThrows(IllegalArgumentException.class, () -> runCode(code));
+    }
+    @Test
+    @DisplayName("Bad weather whatLength() call - invalid type (SafeWord)")
+    void badWeather_whatLengthCall_invalid_type_SafeWord() throws Exception {
+        String input = "\"shrek\"";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // moan hard
+        String srcCode =
+                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
+        JasminBytecode code = this.c.compileString(srcCode, "whatLengthSafeWord");
+
+        // Check that correct exception is thrown
+        assertThrows(IllegalArgumentException.class, () -> runCode(code));
+    }
+
+    @Test
+    @DisplayName("Bad weather whatLength() call - invalid type (bodyCountArray)")
+    void badWeather_whatLengthCall_invalid_type_bodyCountArray() throws Exception {
+        String input = "[6, 9, 6]";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // moan hard
+        String srcCode =
+                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
+        JasminBytecode code = this.c.compileString(srcCode, "whatLengthBodyCountArray");
+
+        // Check that correct exception is thrown
+        assertThrows(IllegalArgumentException.class, () -> runCode(code));
+    }
+
+    @Test
+    @DisplayName("Bad weather whatLength() call - invalid type (lengthArray)")
+    void badWeather_whatLengthCall_invalid_type_lengthArray() throws Exception {
+        String input = "[6.9, 9.6, 6.9]";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // moan hard
+        String srcCode =
+                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
+        JasminBytecode code = this.c.compileString(srcCode, "whatLengthLengthArray");
+
+        // Check that correct exception is thrown
+        assertThrows(IllegalArgumentException.class, () -> runCode(code));
+    }
+
+    @Test
+    @DisplayName("Bad weather whatLength() call - invalid type (SafeWordArray)")
+    void badWeather_whatLengthCall_invalid_type_SafeWordArray() throws Exception {
+        String input = "[\"shrek\", \"fiona\"]";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // moan hard
+        String srcCode =
+                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
+        JasminBytecode code = this.c.compileString(srcCode, "whatLengthSafeWordArray");
+
+        // Check that correct exception is thrown
+        assertThrows(IllegalArgumentException.class, () -> runCode(code));
+    }
+
+    @Test
+    @DisplayName("Bad weather whatLength() call - invalid type (bulgeArray)")
+    void badWeather_whatLengthCall_invalid_type_bulgeArray() throws Exception {
+        String input = "[hard, soft, hard]";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // moan hard
+        String srcCode =
+                MOAN_KEYWORD + " " + WHAT_LENGTH_CALL;
+        JasminBytecode code = this.c.compileString(srcCode, "whatLengthBulgeArray");
+
+        // Check that correct exception is thrown
+        assertThrows(IllegalArgumentException.class, () -> runCode(code));
+    }
+
 }
